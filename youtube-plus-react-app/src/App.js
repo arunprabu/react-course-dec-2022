a8b6ee2d327563c11e6ca2557a20cdff9183675f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-12-28 18:37:36 +0530

    updated

--- youtube-plus-react-app/src/App.js
+++ youtube-plus-react-app/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+// 3 parts 
+/*
+  1. import section
+  2. component definition 
+  3. export 
+*/
 
+import './App.css';
+import Header from './components/Header/Header';
+
+// comp defn 
 function App() {
-  return (
+  // must return JSX
+  return(
     <div>
-      <h1>Success!</h1>
+      <Header></Header>
+      <h1>Success</h1>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ youtube-plus-react-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+// Functional Component with Named Function
+import React from 'react';
+
+function Header() {
+  // must return JSX
+  return(
+    <header>
+      logo comes here
+    </header>
+  )
+}
+
+export default Header;
