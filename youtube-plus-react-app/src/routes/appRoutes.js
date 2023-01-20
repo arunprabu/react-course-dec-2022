7e3b3882afa0b6e851556dfbce438156e633546e
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-01-20 15:12:02 +0530

    updated

--- youtube-plus-react-app/src/routes/appRoutes.js
+++ youtube-plus-react-app/src/routes/appRoutes.js
@@ -1,29 +1,35 @@
 // not a comp 
 
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router';
-import AboutPage from '../pages/AboutPage/AboutPage';
-import ContactUsPage from '../pages/ContactUsPage/ContactUsPage';
-import HocDemoPage from '../pages/HocDemoPage/HocDemoPage';
-import HomePage from '../pages/HomePage/HomePage';
-import StyledComponents from '../pages/StyledComponentsDemoPage/StyledComponentsDemoPage';
-import HooksDemoPage from '../pages/HooksDemoPage/HooksDemoPage';
-import InstaFeedPage from '../pages/InstaFeedPage/InstaFeedPage';
-import ProductsPage from '../pages/ProductsPage/ProductsPage';
-import UnitTestingDemo from '../pages/UnitTestingDemo/UnitTestingDemo';
+
+import HomePage from '../pages/HomePage/HomePage'; // not lazy loaded
+import StyledComponentsDemoPage from '../pages/StyledComponentsDemoPage/StyledComponentsDemoPage'; // not lazy loaded
+
+// the following components are lazy loaded
+const AboutPage = React.lazy( () => import('../pages/AboutPage/AboutPage'));
+const ContactUsPage = React.lazy(() => import('../pages/ContactUsPage/ContactUsPage'));
+const HocDemoPage = React.lazy(() => import('../pages/HocDemoPage/HocDemoPage'));
+const HooksDemoPage = React.lazy(() => import('../pages/HooksDemoPage/HooksDemoPage'));
+const InstaFeedPage = React.lazy(() => import('../pages/InstaFeedPage/InstaFeedPage'));
+const ProductsPage = React.lazy(() => import('../pages/ProductsPage/ProductsPage'));
+const UnitTestingDemo = React.lazy(() => import('../pages/UnitTestingDemo/UnitTestingDemo'));
 
 // organize routes 
 export const appRoutes = (
-  <Routes>
-    {/* URL configuration */}
-    <Route path="/" element={<HomePage />} />
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/contact-us" element={<ContactUsPage />} />
-    <Route path="/styled-comp-demo" element={<StyledComponents />} />
-    <Route path="/insta-feed" element={<InstaFeedPage />} />
-    <Route path="/hoc-demo" element={<HocDemoPage />} />
-    <Route path="/products" element={<ProductsPage />} />
-    <Route path="/hooks-demo" element={<HooksDemoPage />} />
-    <Route path="/unit-testing-demo" element={<UnitTestingDemo />} />
-  </Routes>
+  <Suspense fallback={<div className="spinner-border text-primary"></div>}>
+    <Routes>
+      {/* URL configuration */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact-us" element={<ContactUsPage />} />
+      <Route path="/styled-comp-demo" element={<StyledComponentsDemoPage />} />
+      <Route path="/insta-feed" element={<InstaFeedPage />} />
+      <Route path="/hoc-demo" element={<HocDemoPage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/hooks-demo" element={<HooksDemoPage />} />
+      <Route path="/unit-testing-demo" element={<UnitTestingDemo />} />
+    </Routes>
+  </Suspense>
 );
   
